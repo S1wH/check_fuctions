4b3195c8f8fa05d98170bde99dc155e3fbffc485
S1wH <58308907+S1wH@users.noreply.github.com>
2020-10-22 16:26:26 +0300

    Add files via upload

--- date_autumn.py
+++ date_autumn.py
@@ -7,7 +7,9 @@ def date_autumn(dates):
     date = my_list[0]
     maximum = int(date[0]) * 30 + int(date[1]) + int(date[2]) * 365
     for i in range(len(my_list)):
-        if int(my_list[i][0]) * 30 + int(my_list[i][1]) + int(my_list[i][2]) * 365 > maximum:
-            maximum = int(my_list[i][0]) * 30 + int(my_list[i][1]) + int(my_list[i][2]) * 365
+        if int(my_list[i][0]) * 30 + int(my_list[i][1]) \
+                + int(my_list[i][2]) * 365 > maximum:
+            maximum = int(my_list[i][0]) * 30 + \
+                      int(my_list[i][1]) + int(my_list[i][2]) * 365
             date = my_list[i]
     return '-'.join(date)
